b2691f11d292d256481213ec43daceb3936b8ec0
platote <84842352+platote@users.noreply.github.com>
2022-02-28 17:30:48 +0100

    change z

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export default () => {
   const geometry = new THREE.BoxGeometry(1,1,1);
   const material = new THREE.MeshBasicMaterial({color: 0xff0000});
   const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(2,10,0);
+  mesh.position.set(2,10,5);
   scene.add(mesh);
   app.updateMatrixWorld();
   return mesh;
