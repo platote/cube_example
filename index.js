c188182e6aa649663b42d6176e9a558581d5cd8c
platote <84842352+platote@users.noreply.github.com>
2022-02-28 16:38:09 +0100

    try with updatematrixworld

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useScene} = metaversefile;
+const {useApp, useScene} = metaversefile;
 
 export default () => {
   const scene = useScene();
+  const app = useApp();
   const geometry = new THREE.BoxGeometry(1,1,1);
   const material = new THREE.MeshBasicMaterial({color: 0xff0000});
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(2,2,0);
   scene.add(mesh);
+  app.updateMatrixWorld();
   return mesh;
 };
