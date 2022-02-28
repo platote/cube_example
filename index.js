d4240c02a4c96c9f00a059db59b46747cc207e81
platote <84842352+platote@users.noreply.github.com>
2022-02-28 15:56:37 +0100

    update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import metaversefile from 'metaversefile'
 const {useApp} = metaversefile
 
 export default () => {
-  const app = useApp();
+  const app = useApp()
   const geometry = new THREE.BoxGeometry(1,1,1)
   const material = new THREE.MeshBasicMaterial({color: 0xff0000})
   const mesh = new THREE.Mesh(geometry, material)
   mesh.position.y = 1
   app.add(mesh)
+}
   
