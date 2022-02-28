bfb2c999257ec094eb0a7115316bdc074e3ffc0c
platote <84842352+platote@users.noreply.github.com>
2022-02-28 16:28:31 +0100

    app for scene

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp} = metaversefile;
+const {useScene} = metaversefile;
 
 export default () => {
-  const app = useApp();
+  const scene = useScene();
   const geometry = new THREE.BoxGeometry(1,1,1);
   const material = new THREE.MeshBasicMaterial({color: 0xff0000});
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.y = 10;
-  app.add(mesh);
+  scene.add(mesh);
   return mesh;
 };
