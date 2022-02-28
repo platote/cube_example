40c4cd9097583ccc2774402276bcfecb72c035f5
platote <84842352+platote@users.noreply.github.com>
2022-02-28 16:33:11 +0100

    d

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export default () => {
   const geometry = new THREE.BoxGeometry(1,1,1);
   const material = new THREE.MeshBasicMaterial({color: 0xff0000});
   const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.y = 10;
+  mesh.position.set(2,2,0);
   scene.add(mesh);
   return mesh;
 };
